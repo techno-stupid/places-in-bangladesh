22eb76bb9504506824deab8bb0c9e9ce7cdf755b
Mahady Hasan Pial <pial.coder@gmail.com>
2023-09-02 14:20:38 +0600

    Divisions, Districts, Upazilas and Unions Added

---
+++ divisions/divisions.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Division struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	BnName string `json:"bn_name"`
+	URL    string `json:"url"`
+}
+
+func main() {
+	jsonData := `[{"id":"1","name":"Chattagram","bn_name":"চট্টগ্রাম","url":"www.chittagongdiv.gov.bd"},
+	{"id":"2","name":"Rajshahi","bn_name":"রাজশাহী","url":"www.rajshahidiv.gov.bd"},
+	{"id":"3","name":"Khulna","bn_name":"খুলনা","url":"www.khulnadiv.gov.bd"},
+	{"id":"4","name":"Barisal","bn_name":"বরিশাল","url":"www.barisaldiv.gov.bd"},
+	{"id":"5","name":"Sylhet","bn_name":"সিলেট","url":"www.sylhetdiv.gov.bd"},
+	{"id":"6","name":"Dhaka","bn_name":"ঢাকা","url":"www.dhakadiv.gov.bd"},
+	{"id":"7","name":"Rangpur","bn_name":"রংপুর","url":"www.rangpurdiv.gov.bd"},
+	{"id":"8","name":"Mymensingh","bn_name":"ময়মনসিংহ","url":"www.mymensinghdiv.gov.bd"}]`
+
+	var divisions []Division
+	err := json.Unmarshal([]byte(jsonData), &divisions)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(divisions)
+}
---
+++ divisions/divisions.sql
@@ -0,0 +1,58 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+-- Database: `places-in-bangladesh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` int(1) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `bn_name` varchar(25) NOT NULL,
+  `url` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `name`, `bn_name`, `url`) VALUES
+(1, 'Chattagram', 'চট্টগ্রাম', 'www.chittagongdiv.gov.bd'),
+(2, 'Rajshahi', 'রাজশাহী', 'www.rajshahidiv.gov.bd'),
+(3, 'Khulna', 'খুলনা', 'www.khulnadiv.gov.bd'),
+(4, 'Barisal', 'বরিশাল', 'www.barisaldiv.gov.bd'),
+(5, 'Sylhet', 'সিলেট', 'www.sylhetdiv.gov.bd'),
+(6, 'Dhaka', 'ঢাকা', 'www.dhakadiv.gov.bd'),
+(7, 'Rangpur', 'রংপুর', 'www.rangpurdiv.gov.bd'),
+(8, 'Mymensingh', 'ময়মনসিংহ', 'www.mymensinghdiv.gov.bd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
